03b9a9d7f5face419e98ccfb5da30e00e180068b
sergii.momot <srg.momot@gmail.com>
2024-01-15 17:36:41 +0200

    Lesson 15 - Create animation for Bird, create logic for show hit sprite, correction scrip for control bird

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,29 +1,36 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
 
-public class Bird : MonoBehaviour {
+public class Bird : MonoBehaviour
+{
+    public float speed = 50f;
+    public GameObject hitSprite;
 
-    public Sprite deathSprite;
+    [Header("Events")] public UnityEvent deathEvent;
 
-    [Header("Events")]
-    public UnityEvent deathEvent;
-    
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D rigidbody;
     private bool isAlive = true;
 
     private bool screenClick = false;
 
-    private void Start () {
+    private Animator animator;
+
+    private void Start()
+    {
         rigidbody = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
-    private void Update () {
+    private void Update()
+    {
         if ((Input.GetKeyDown(KeyCode.Space) || screenClick == true) && isAlive)
         {
             rigidbody.velocity = Vector2.zero;
-            rigidbody.AddForce(new Vector2(0.5f, 1) * 50f);
+            rigidbody.AddForce(new Vector2(0.5f, 1) * speed);
+
+            animator.SetTrigger("Flip");
 
             screenClick = false;
         }
@@ -34,11 +41,27 @@ public class Bird : MonoBehaviour {
         if (collision.gameObject.tag == "Wall")
         {
             isAlive = false;
-            spriteRenderer.sprite = deathSprite;
             rigidbody.velocity = Vector2.down;
-            
+
+            animator.SetTrigger("Death");
+
+            ShowHitSprite(collision.contacts[0].point);
+
             deathEvent?.Invoke();
         }
+    }
+
+    private void ShowHitSprite(Vector2 position)
+    {
+        hitSprite.SetActive(true);
+        hitSprite.transform.position = position;
+
+        Invoke(nameof(HideHitSprite), 0.2f);
+    }
+
+    private void HideHitSprite()
+    {
+        hitSprite.SetActive(false);
     }
 
     public void ClickScreenButton()
