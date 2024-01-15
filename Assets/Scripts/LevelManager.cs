af7d014098dddd20ebb20ab245d025d05fed22bb
sergii.momot <srg.momot@gmail.com>
2024-01-15 19:39:22 +0200

    Lesson 20 - Add game-over animated panel

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -21,7 +21,7 @@ public class LevelManager : MonoBehaviour
         SceneManager.LoadScene(nameLevel);
     }
 
-    private void LoadLevelList()
+    public void LoadLevelList()
     {
         LoadLevel("LevelList");
     }
